98d217598ff84aff0e6867fe94fbb66905ad2afa
_sudo11 <rayanmajdi42@outlook.com>
2024-11-21 17:52:47 -0500

    blank3r abilities

---
+++ script.js
@@ -0,0 +1,33 @@
+function blank3r(furl = document.getElementById('blank3rVal').value) {  
+    var win = window.open();
+    win.document.body.style.margin = '0';
+    win.document.body.style.height = '100vh';
+    var title = win.document.createElement('title');
+    var div = win.document.createElement('div');
+    var iframe = win.document.createElement('iframe');
+    var favicon = win.document.createElement("link");
+    title.innerHTML = "Home | Schoology";
+    favicon.rel = 'shortcut icon';
+    favicon.type = 'image/x-icon';
+    favicon.href = 'https://asset-cdn.schoology.com/sites/all/themes/schoology_theme/favicon.ico';
+    iframe.style.border = 'none';
+    iframe.style.width = '100vw';
+    iframe.style.height = '100vh';
+    iframe.style.margin = '0';
+    console.log('furl' + furl);
+    iframe.src = furl;
+    div.innerHTML = '<title>Home | Schoology</title>';
+    div.appendChild(iframe);
+    div.appendChild(favicon);
+    win.document.head.appendChild(favicon);
+    win.document.body.appendChild(div);
+    win.onbeforeunload = function () {
+        return false;
+    }
+    win.focus();
+}
+window.addEventListener(onload, function (){
+    if(this.window.location.href != 'about:blank'){
+        blank3r('https://' + this.window.location.href);
+    }   
+})
