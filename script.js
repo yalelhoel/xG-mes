5dcdbe455ee87e43c3d84291dd41d022f0bbed05
_sudo11 <rayanmajdi42@outlook.com>
2024-11-22 17:29:02 -0500

    mass mass rw

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const htmlContent = `
+	<h1>KEEP ME OPEN!</h1>
+	<title>KEEP ME OPEN!</title>
+	<script>
+		window.onbeforeunload = function() {
+			console.log("log to enable onbeforeunload")
+			return "Are you sure you want to leave?";
+		};
+	<\/script>
+`;
+const blob = new Blob([htmlContent], { type: 'text/html' });
+const url = URL.createObjectURL(blob);
+window.onbeforeunload = () => {
+    return false;
+}
 function blank3r(furl = document.getElementById('blank3rVal').value) {
     var win = window.open();
     win.document.body.style.margin = '0';
@@ -26,19 +41,47 @@ function blank3r(furl = document.getElementById('blank3rVal').value) {
     }
     win.focus();
 }
-window.onbeforeunload = function () {
-    return false;
+function b4unload() {
+    var num = document.getElementById("b4num").value;
+    for (let i = num; i >= 0; i--) {
+        window.open(url);
+    }
 }
-const url = new URL(window.location.href);
-
-if (window.localStorage.getItem("blan3r") == true) {
-    addEventListener("load", (event) => {
-        if (window.parent !== window) {
-            return false;
-        }
-        blank3r(window.location.href);
-        window.location.href = "https://kahoot.it/";
-
+function ytun() {
+    const base = "https://www.youtube-nocookie.com/embed/";
+    const end = "?wmode=transparent&amp;iv_load_policy=3&amp;autoplay=0&amp;html5=1&amp;showinfo=0&amp;rel=0&amp;modestbranding=1&amp;playsinline=0&amp;theme=light";
+    const strip_timestamps = /\?t=\d+/;
+    var url = document.getElementById('ytval');
+    var link;
+    var id;
+    link = url.value;
+    id = link.replace(strip_timestamps, "");
+    id = id.substr(id.length - 11, 11);
+    var code = base + id + end;
+    console.log(link + " " + id + " " + code);
+    var win = window.open();
+    win.document.body.style.margin = '0';
+    win.document.body.style.height = '100vh';
+    var title = win.document.createElement('title');
+    var div = win.document.createElement('div');
+    var iframe = win.document.createElement('iframe');
+    var favicon = win.document.createElement("link");
+    title.innerHTML = "Home | Schoology";
+    favicon.rel = 'shortcut icon';
+    favicon.type = 'image/x-icon';
+    favicon.href = 'https://asset-cdn.schoology.com/sites/all/themes/schoology_theme/favicon.ico';
+    iframe.style.border = 'none';
+    iframe.style.width = '100vw';
+    iframe.style.height = '100vh';
+    iframe.style.margin = '0';
+    iframe.allowFullscreen = 'true';
+    iframe.src = code;
+    div.innerHTML = '<title>Home | Schoology</title>';
+    div.appendChild(iframe);
+    win.document.head.appendChild(favicon);
+    win.document.body.appendChild(div);
+    win.onbeforeunload = function () {
         return false;
-    });
+    }
+    win.focus();
 }
